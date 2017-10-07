f452bd256a5f752114c6f0bb6225c400fa060959
Rich Franzmeier <rfranzmeier@intertech.com>
2017-10-07 15:25:12 -0500

    Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AppLoadModule } from './app-load/app-load.module';
-=======
->>>>>>> 50cec3a65f58ed6a7e6a172fd053b1ed69cde11c
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-<<<<<<< HEAD
     BrowserModule,
     AppLoadModule
-=======
-    BrowserModule
->>>>>>> 50cec3a65f58ed6a7e6a172fd053b1ed69cde11c
   ],
   providers: [],
   bootstrap: [AppComponent]
